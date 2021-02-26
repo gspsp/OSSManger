a6cced102e2365592ce0993f2485117e4a8cef45
ihesro <2770520782@qq.com>
2021-02-26 23:42:58 +0800

    1.0.0

--- Plugin.php
+++ Plugin.php
@@ -19,7 +19,14 @@ class OSSManger_Plugin implements Typecho_Plugin_Interface
 	 */
 	public static function activate()
 	{
-
+		$F = DIRECTORY_SEPARATOR;
+		$G = realpath(__DIR__ . "${F}..${F}..${F}..${F}") . "${F}";
+		$R = __DIR__ . "${F}static${F}";
+		//替换file-upload.php
+		if (!file_exists("${G}admin${F}file-upload.php.old.OSSManger")) {
+			rename("${G}admin${F}file-upload.php", "${G}admin${F}file-upload.php.old.OSSManger");
+			copy("${R}file-upload.php", "${G}admin${F}file-upload.php");
+		}
 		Typecho_Plugin::factory('admin/menu.php')->navBar = array('OSSManger_Plugin', 'render');
 	}
 
@@ -33,6 +40,11 @@ class OSSManger_Plugin implements Typecho_Plugin_Interface
 	 */
 	public static function deactivate()
 	{
+		$F = DIRECTORY_SEPARATOR;
+		$G = realpath(__DIR__ . "${F}..${F}..${F}..${F}") . "${F}";
+		//还原file-upload.php
+		unlink("${G}admin${F}file-upload.php");
+		rename("${G}admin${F}file-upload.php.old.OSSManger", "${G}admin${F}file-upload.php");
 	}
 	/**
 	 * 获取插件配置面板
@@ -43,27 +55,6 @@ class OSSManger_Plugin implements Typecho_Plugin_Interface
 	 */
 	public static function config(Typecho_Widget_Helper_Form $form)
 	{
-		$F = DIRECTORY_SEPARATOR;
-		$G = realpath(__DIR__ . "${F}..${F}..${F}..${F}") . "${F}";
-		$R = __DIR__ . "${F}static${F}";
-		$success = true;
-		if (isset($_GET['action'])) {
-			switch ($_GET['action']) {
-				case 'replace':
-					$success = $success && rename("${G}admin${F}file-upload.php", "${G}admin${F}file-upload.php.old");
-					$success = $success && copy("${R}static${F}file-upload.php", "${G}admin${F}file-upload.php");
-					break;
-				case 'restore':
-					$success = $success && unlink("${G}admin${F}file-upload.php");
-					$success = $success && rename("${G}static${F}file-upload.php.old", "${G}admin${F}file-upload.php");
-					break;
-			}
-			if ($success) {
-				Typecho_Widget::widget('Widget_Notice')->set(_t("操作失败，admin/file-upload.php没有写入权限"), 'error');
-			} else {
-				Typecho_Widget::widget('Widget_Notice')->set(_t("操作成功！"), 'success');
-			}
-		}
 		//https://oss.console.aliyun.com/bucket/oss-cn-beijing/lsky-sp/object
 		$region = new Typecho_Widget_Helper_Form_Element_Text('region', null, null, _t('region:'));
 		$form->addInput($region->addRule('required', _t('不能为空！')));
@@ -79,20 +70,6 @@ class OSSManger_Plugin implements Typecho_Plugin_Interface
 
 		$selfDomain = new Typecho_Widget_Helper_Form_Element_Text('selfDomain', null, null, _t('selfDomain:'));
 		$form->addInput($selfDomain);
-
-		$replaceBtn = new Typecho_Widget_Helper_Form_Element_Submit();
-		$replaceBtn->value(_t('替换file-upload.php'));
-		$replaceBtn->description(_t('通常只需要在第一次启用插件的时候，手动点击该按钮。'));
-		$replaceBtn->input->setAttribute('class', 'btn btn-s btn-warn btn-operate');
-		$replaceBtn->input->setAttribute('formaction', Typecho_Common::url('/options-plugin.php?config=OSSManger&action=replace', Helper::options()->adminUrl));
-		$form->addItem($replaceBtn);
-
-		$restoreBtn = new Typecho_Widget_Helper_Form_Element_Submit();
-		$restoreBtn->value(_t('还原file-upload.php'));
-		$restoreBtn->description(_t('禁用插件之前需手动点击此按钮。'));
-		$restoreBtn->input->setAttribute('class', 'btn btn-s btn-warn btn-operate');
-		$restoreBtn->input->setAttribute('formaction', Typecho_Common::url('/options-plugin.php?config=OSSManger&action=restore', Helper::options()->adminUrl));
-		$form->addItem($restoreBtn);
 	}
 
 	/**
